f78909dadd27a7330fa6086b139fb3c6932e0d1e
NakagamiYuta <guguru0014@gmail.com>
2022-06-25 18:56:16 +0900

    [change](sysdc/tokenize) Token構造体の作成方法をnewからfromへ変更

--- src/sysdc/tokenize/token.rs
+++ src/sysdc/tokenize/token.rs
@@ -1,3 +1,5 @@
+#[derive(Debug)]
+#[derive(PartialEq)]
 pub enum TokenKind {
     /* Reserved */
     Layer,              // layer
@@ -28,6 +30,7 @@ pub enum TokenKind {
     Number
 }
 
+#[derive(Debug)]
 pub struct Token {
     pub kind: TokenKind,
     orig_id: Option<String>,
@@ -35,28 +38,44 @@ pub struct Token {
 }
 
 impl Token {
-    pub fn new(kind: TokenKind) -> Token {
-        Token {
-            kind,
-            orig_id: None,
-            orig_number: None
-        }
+    pub fn from_string(orig: String) -> Token {
+        let kind = match orig.as_str() {
+            "layer"     => TokenKind::Layer,
+            "ref"       => TokenKind::Ref,
+            "data"      => TokenKind::Data,
+            "module"    => TokenKind::Module,
+            "use"       => TokenKind::Use,
+            "modify"    => TokenKind::Modify,
+            "link"      => TokenKind::Link,
+            "branch"    => TokenKind::Branch,
+            "chain"     => TokenKind::Chain,
+            "->"        => TokenKind::Allow,
+            ":"         => TokenKind::Mapping,
+            "="         => TokenKind::Equal,
+            "."         => TokenKind::Accessor,
+            "::"        => TokenKind::PAccessor,
+            ","         => TokenKind::Separater,
+            ";"         => TokenKind::Semicolon,
+            "("         => TokenKind::ParenthesisBegin,
+            ")"         => TokenKind::ParenthesisEnd,
+            "{"         => TokenKind::BracketBegin,
+            "}"         => TokenKind::BracketEnd,
+            _           => TokenKind::Identifier
+        };
+        let orig_id = match kind {
+            TokenKind::Identifier => Some(orig),
+            _ => None
+        };
+
+        Token { kind, orig_id, orig_number: None }
     }
 
-    pub fn new_id(orig: String) -> Token {
-        Token {
-            kind: TokenKind::Identifier,
-            orig_id: Some(orig),
-            orig_number: None
-        }
-    }
-
-    pub fn new_number(orig: i32) -> Token {
+    pub fn from_i32(orig: i32) -> Token {
         Token {
             kind: TokenKind::Number,
             orig_id: None,
             orig_number: Some(orig)
-        } 
+        }
     }
 
     pub fn get_id(&self) -> String {
@@ -79,38 +98,67 @@ mod test {
     use super::{ Token, TokenKind };
 
     #[test]
+    fn create_token_from_string() {
+        let str_kind_mapping = [
+            ("layer",   TokenKind::Layer),
+            ("ref",     TokenKind::Ref),
+            ("data",    TokenKind::Data),
+            ("module",  TokenKind::Module),
+            ("use",     TokenKind::Use),
+            ("modify",  TokenKind::Modify),
+            ("link",    TokenKind::Link),
+            ("branch",  TokenKind::Branch),
+            ("chain",   TokenKind::Chain),
+            ("->",      TokenKind::Allow),
+            (":",       TokenKind::Mapping),
+            ("=",       TokenKind::Equal),
+            (".",       TokenKind::Accessor),
+            ("::",      TokenKind::PAccessor),
+            (",",       TokenKind::Separater),
+            (";",       TokenKind::Semicolon),
+            ("(",       TokenKind::ParenthesisBegin),
+            (")",       TokenKind::ParenthesisEnd),
+            ("{",       TokenKind::BracketBegin),
+            ("}",       TokenKind::BracketEnd),
+        ];
+        for (_str, kind) in str_kind_mapping {
+            assert_eq!(Token::from_string(_str.to_string()).kind, kind);
+        }
+    }
+
+    #[test]
     #[should_panic]
     fn get_identifer_from_reserved_token() {
-        Token::new(TokenKind::Accessor).get_id();
+        Token::from_string("->".to_string()).get_id();
     }
 
     #[test]
     #[should_panic]
     fn get_number_from_reserved_token() {
-        Token::new(TokenKind::Accessor).get_number();
+        Token::from_string("->".to_string()).get_number();
     }
 
     #[test]
     fn get_identifer_from_identifer_token() {
-        let id = Token::new_id("test".to_string()).get_id();
+        let id = Token::from_string("test".to_string()).get_id();
         assert_eq!(id, "test");
     }
 
     #[test]
     #[should_panic]
     fn get_number_from_identifer_token() {
-        Token::new_id("test".to_string()).get_number();
+        Token::from_string("test".to_string()).get_number();
     }
 
     #[test]
     #[should_panic]
     fn get_identifer_from_number_token() {
-        Token::new_number(1204).get_id();
+        Token::from_i32(1204).get_id();
     }
 
     #[test]
     fn get_number_from_number_token() {
-        let number = Token::new_number(1204).get_number();
+        let number = Token::from_i32(1204).get_number();
         assert_eq!(number, 1204);
     }
 }
