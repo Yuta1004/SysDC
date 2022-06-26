8fc06520cacabf55225e007f3edf68e3fbf84f03
NakagamiYuta <guguru0014@gmail.com>
2022-06-26 20:20:32 +0900

    [change](compiler/structure) SysDC**の定義にNameを使用するようにした

--- src/compiler/structure.rs
+++ src/compiler/structure.rs
@@ -1,66 +1,62 @@
 use std::rc::Rc;
 use std::collections::HashMap;
 
+use super::name::Name;
 use super::types::SysDCType;
 
 pub struct SysDCSystem {
+    name: Name,
     layers: Vec<SysDCLayer>
 }
 
 pub struct SysDCLayer {
-    name: String,
-    namespace: String,
+    name: Name,
     units: Vec<SysDCUnit>
 }
 
 pub struct SysDCUnit {
-    name: String,
-    namespace: String,
+    name: Name,
     data: Vec<Rc<SysDCData>>,
     modules: Vec<Rc<SysDCModule>>
 }
 
 pub struct SysDCData {
-    name: String,
-    namespace: String,
+    name: Name,
     variables: Vec<Rc<SysDCVariable>>
 }
 
 impl SysDCType for SysDCData {
     fn get_name(&self) -> String {
-        self.name.clone()
+        self.name.get_name()
     }
 
     fn get_full_name(&self) -> String {
-        self.namespace.clone() + &self.name
+        self.name.get_full_name()
     }
 }
 
 pub struct SysDCVariable {
-    name: String,
-    namespace: String,
+    name: Name,
     var_type: Vec<Rc<dyn SysDCType>>
 }
 
 impl SysDCType for SysDCVariable {
     fn get_name(&self) -> String {
-        self.name.clone()
+        self.name.get_name()
     }
 
     fn get_full_name(&self) -> String {
-        self.namespace.clone() + &self.name
+        self.name.get_full_name()
     }
 }
 
 pub struct SysDCModule {
-    name: String,
-    namespace: String,
+    name: Name,
     procedures: Vec<Rc<SysDCProcedure>>
 }
 
 pub struct SysDCProcedure {
-    name: String,
-    namespace: String,
+    name: Name,
     uses: Vec<Rc<SysDCVariable>>,
     modifies: Vec<Rc<SysDCVariable>>,
     links: Vec<Rc<SysDCLink>>
@@ -72,13 +68,13 @@ pub enum SysDCLinkType {
 }
 
 pub struct SysDCLink {
-    namespace: String,
+    name: Name,
     link_type: SysDCLinkType,
     instances: Vec<Rc<SysDCInstanceOfProcedure>>
 }
 
 pub struct SysDCInstanceOfProcedure {
-    namespace: String,
+    name: Name,
     procedure: Rc<SysDCProcedure>,
     var_mapping: HashMap<String, Rc<SysDCVariable>>
 }
