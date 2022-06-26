94e5e8715c2948817b0fa7e559dcbbafaee7d611
NakagamiYuta <guguru0014@gmail.com>
2022-06-26 20:30:58 +0900

    [change](compiler/name) Name::from -> Name::new

--- src/compiler/name.rs
+++ src/compiler/name.rs
@@ -4,27 +4,17 @@ pub struct Name {
 }
 
 impl Name {
-    pub fn new(name: String, namespace: String) -> Name {
-        let last_idx = namespace.len()-1;
-        let namespace = match namespace.chars().nth(last_idx).unwrap() {
-            '.' => namespace,
-            _ => namespace + "."
-        };
-
-        Name { name, namespace }
+    pub fn new(base: Name, name: String) -> Name {
+        Name {
+            name,
+            namespace: base.get_full_name() + "."
+        }
     }
 
     pub fn new_root() -> Name {
         Name {
             name: "0".to_string(),
             namespace: ".".to_string()
-        }
-    }
-
-    pub fn from(base: Name, name: String) -> Name {
-        Name {
-            name,
-            namespace: base.get_full_name() + "."
         }
     }
 
@@ -43,20 +33,12 @@ mod test {
 
     #[test]
     fn create_name() {
-        let name = Name::new("aaa".to_string(), ".".to_string());
+        let name = Name::new(Name::new_root(), "aaa".to_string());
         assert_eq!(name.get_name(), "aaa".to_string());
-        assert_eq!(name.get_full_name(), ".aaa".to_string());
-
-        let name = Name::new("aaa".to_string(), ".test".to_string());
-        assert_eq!(name.get_name(), "aaa".to_string());
-        assert_eq!(name.get_full_name(), ".test.aaa".to_string());
+        assert_eq!(name.get_full_name(), ".0.aaa".to_string());
 
         let name = Name::new_root();
         assert_eq!(name.get_name(), "0".to_string());
         assert_eq!(name.get_full_name(), ".0".to_string());
-
-        let name = Name::from(Name::new_root(), "aaa".to_string());
-        assert_eq!(name.get_name(), "aaa".to_string());
-        assert_eq!(name.get_full_name(), ".0.aaa".to_string());
     }
 }
