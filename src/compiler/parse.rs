4bdee8d952e9d78cffdb286a476e49fb37fc859e
NakagamiYuta <guguru0014@gmail.com>
2022-06-28 16:16:10 +0900

    [add](compiler/parse) 空文字列をパースできるようにした

--- src/compiler/parse.rs
+++ src/compiler/parse.rs
@@ -1,3 +1,4 @@
+use super::structure::{ SysDCSystem };
 use super::token::Tokenizer;
 
 pub struct Parser<'a> {
@@ -7,5 +8,28 @@ pub struct Parser<'a> {
 impl<'a> Parser<'a> {
     pub fn new(tokenizer: Tokenizer<'a>) -> Parser<'a> {
         Parser { tokenizer }
+    }
+
+    pub fn parse(&mut self) -> SysDCSystem {
+        let system = SysDCSystem::new();
+        system
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{ Tokenizer, Parser };
+    use super::{ SysDCSystem };
+
+    #[test]
+    fn parse_system() {
+        let program = "".to_string();
+        let tokenizer = Tokenizer::new(&program);
+        let mut parser = Parser::new(tokenizer);
+        compare_system(parser.parse(), SysDCSystem::new());
+    }
+
+    fn compare_system(a: SysDCSystem, b: SysDCSystem) {
+        assert_eq!(format!("{:?}", a), format!("{:?}", b));
     }
 }
