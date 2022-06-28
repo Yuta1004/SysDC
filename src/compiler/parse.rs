5122060bcf583dcbfe9fbfe9145b53532b927fe6
NakagamiYuta <guguru0014@gmail.com>
2022-06-28 17:05:46 +0900

    [change](compiler/parser) Parser::parseがSysDCUnitを返すようにした

--- src/compiler/parse.rs
+++ src/compiler/parse.rs
@@ -1,28 +1,35 @@
 use super::name::Name;
 use super::token::{ Token, TokenKind, Tokenizer };
-use super::structure::{ SysDCSystem, SysDCLayer };
+use super::structure::{ SysDCSystem, SysDCLayer, SysDCUnit };
 
 pub struct Parser<'a> {
-    name: String,
+    pub namespace: Name,
+    pub unit_name: String,
+    pub layer_num: i32,
     tokenizer: Tokenizer<'a>
 }
 
 impl<'a> Parser<'a> {
-    pub fn new(name: String, tokenizer: Tokenizer<'a>) -> Parser<'a> {
-        Parser { name, tokenizer }
+    pub fn new(namespace: &Name, unit_name: &String, tokenizer: Tokenizer<'a>) -> Parser<'a> {
+        Parser {
+            namespace: namespace.clone(),
+            unit_name: unit_name.clone(),
+            layer_num: 0,
+            tokenizer
+        }
     }
 
-    pub fn parse(&mut self) -> SysDCSystem {
-        let mut system = SysDCSystem::new();
-        system.push_layer(self.parse_layer(&system.name));
-        system
+    pub fn parse(&mut self) -> SysDCUnit {
+        let layer = self.parse_layer(&self.namespace.clone());
+        let mut unit = SysDCUnit::new(&layer.name, &self.unit_name);
+        unit
     }
 
     fn parse_layer(&mut self, namespace: &Name) -> SysDCLayer {
         self.expect(TokenKind::Layer);
         let num_token = self.expect(TokenKind::Number);
         self.expect(TokenKind::Semicolon);
-        SysDCLayer::new(namespace, num_token.get_number())
+        SysDCLayer::new(&namespace, num_token.get_number())
     }
 
     fn expect(&mut self, kind: TokenKind) -> Token {
@@ -35,22 +42,33 @@ impl<'a> Parser<'a> {
 
 #[cfg(test)]
 mod test {
+    use super::Name;
     use super::{ Tokenizer, Parser };
-    use super::{ SysDCSystem, SysDCLayer };
+    use super::{ SysDCSystem, SysDCLayer, SysDCUnit };
 
     #[test]
-    fn parse_system() {
-        let program = "layer 0;".to_string();
+    fn parse_simple_unit() {
+        let program = "layer 0;";
 
-        let mut system = SysDCSystem::new();
-        system.push_layer(SysDCLayer::new(&system.name, 0));
+        let unit = generate_test_unit(0);
 
-        let tokenizer = Tokenizer::new(&program);
-        let mut parser = Parser::new("test".to_string(), tokenizer);
-        compare_system(parser.parse(), system);
+        compare_unit(parse(program), unit);
     }
 
-    fn compare_system(a: SysDCSystem, b: SysDCSystem) {
+    fn compare_unit(a: SysDCUnit, b: SysDCUnit) {
         assert_eq!(format!("{:?}", a), format!("{:?}", b));
+    }
+
+    fn generate_test_unit(layer_num: i32) -> SysDCUnit {
+        let root_namespace = Name::new_root();
+        let layer_namespace = Name::new(&root_namespace, &format!("layer{}", layer_num));
+        SysDCUnit::new(&layer_namespace, &"test".to_string()) 
+    }
+
+    fn parse(program: &str) -> SysDCUnit {
+        let program = program.to_string();
+        let tokenizer = Tokenizer::new(&program);
+        let mut parser = Parser::new(&Name::new_root(), &"test".to_string(), tokenizer);
+        parser.parse()
     }
 }
