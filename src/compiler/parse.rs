27e9a395239967978879ad38e08dbcccc947cd04
NakagamiYuta <guguru0014@gmail.com>
2022-06-28 16:40:19 +0900

    [add](compiler/parse) Layerをパースできるようにした

--- src/compiler/parse.rs
+++ src/compiler/parse.rs
@@ -1,32 +1,53 @@
-use super::structure::{ SysDCSystem };
-use super::token::Tokenizer;
+use super::name::Name;
+use super::token::{ Token, TokenKind, Tokenizer };
+use super::structure::{ SysDCSystem, SysDCLayer };
 
 pub struct Parser<'a> {
+    name: String,
     tokenizer: Tokenizer<'a>
 }
 
 impl<'a> Parser<'a> {
-    pub fn new(tokenizer: Tokenizer<'a>) -> Parser<'a> {
-        Parser { tokenizer }
+    pub fn new(name: String, tokenizer: Tokenizer<'a>) -> Parser<'a> {
+        Parser { name, tokenizer }
     }
 
     pub fn parse(&mut self) -> SysDCSystem {
-        let system = SysDCSystem::new();
+        let mut system = SysDCSystem::new();
+        system.push_layer(self.parse_layer(&system.name));
         system
+    }
+
+    fn parse_layer(&mut self, namespace: &Name) -> SysDCLayer {
+        self.expect(TokenKind::Layer);
+        let num_token = self.expect(TokenKind::Number);
+        self.expect(TokenKind::Semicolon);
+        SysDCLayer::new(namespace, num_token.get_number())
+    }
+
+    fn expect(&mut self, kind: TokenKind) -> Token {
+        match self.tokenizer.expect_kind(kind.clone()) {
+            Some(token) => token,
+            None => panic!("[ERROR] Token {:?} is expected, but not found.", kind)
+        }
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::{ Tokenizer, Parser };
-    use super::{ SysDCSystem };
+    use super::{ SysDCSystem, SysDCLayer };
 
     #[test]
     fn parse_system() {
-        let program = "".to_string();
+        let program = "layer 0;".to_string();
+
+        let mut system = SysDCSystem::new();
+        system.push_layer(SysDCLayer::new(&system.name, 0));
+
         let tokenizer = Tokenizer::new(&program);
-        let mut parser = Parser::new(tokenizer);
-        compare_system(parser.parse(), SysDCSystem::new());
+        let mut parser = Parser::new("test".to_string(), tokenizer);
+        compare_system(parser.parse(), system);
     }
 
     fn compare_system(a: SysDCSystem, b: SysDCSystem) {
