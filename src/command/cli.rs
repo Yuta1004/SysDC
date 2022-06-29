582749fb17d2a61206d9487112ed0278f22466f1
NakagamiYuta <guguru0014@gmail.com>
2022-06-30 02:39:52 +0900

    [add](command/cli) 雑にCLIを書いた(コンパイル処理以外)

--- src/command/cli.rs
+++ src/command/cli.rs
@@ -1,8 +1,74 @@
+use std::io;
+use std::io::Write;
+
+use crate::compiler::structure::SysDCSystem;
+use crate::plugin::PluginManager;
+
 #[derive(clap::Parser)]
 pub struct CliCmd;
 
 impl CliCmd {
-    pub fn run(&self) {}
+    pub fn run(&self) {
+        let system = SysDCSystem::new();
+        let plugin_manager = PluginManager::new();
+
+        loop {
+            print!("> ");
+            io::stdout().flush().unwrap(); 
+
+            let mut text = String::new();
+            if let Err(e) = io::stdin().read_line(&mut text) {
+                println!("UnknownError: {}\n", e);
+                continue;
+            }
+            let text = match text.trim() {
+                "exit" => break,
+                text => text.to_string()
+            };
+
+            let (cmd, name, args) = match CliCmd::parse_cli_text(text) {
+                Some(result) => result,
+                None => {
+                    println!("SyntaxError: Usage => in/out <name> <args>\n");
+                    continue;
+                }
+            };
+
+            match cmd.as_str() {
+                "in" => CliCmd::run_mode_in(&plugin_manager, name, args),
+                "out" => CliCmd::run_mode_out(&plugin_manager, name, args, &system),
+                _ => {
+                    println!("SyntaxError: Usage => in/out {} <args>\n", cmd);
+                    continue;
+                }
+            }
+        }
+        println!("Bye...");
+    }
+ 
+    fn run_mode_in(plugin_manager: &PluginManager, name: String, args: Vec<String>) {
+        let plugin = match plugin_manager.get_type_in(&name) {
+            Some(plugin) => plugin,
+            None => {
+                println!("PluginError: {} not found\n", name);
+                return;
+            }
+        };
+        for (unit_name, program) in plugin.run(args) {
+            println!("{}, {}", unit_name, program);
+        }
+    }
+
+    fn run_mode_out(plugin_manager: &PluginManager, name: String, args: Vec<String>, system: &SysDCSystem) {
+        let plugin = match plugin_manager.get_type_out(&name) {
+            Some(plugin) => plugin,
+            None => {
+                println!("PluginError: {} not found\n", name);
+                return;
+            }
+        };
+        plugin.run(args, system);
+    }
 
     fn parse_cli_text(text: String) -> Option<(String, String, Vec<String>)> {
         let splitted_text = text.split(" ").map(|s| s.to_string()).collect::<Vec<String>>();
