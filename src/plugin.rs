a90a82c226e583d48921c992f814cc46a22a85c1
NakagamiYuta <guguru0014@gmail.com>
2022-06-30 01:20:38 +0900

    [add](plugin) PluginManager.get_type_in, get_type_outを実装

--- src/plugin.rs
+++ src/plugin.rs
@@ -24,6 +24,24 @@ impl PluginManager {
         PluginManager { in_plugins, out_plugins }
     }
 
+    pub fn get_type_in(&self, name: &String) -> Option<&Box<dyn InputPlugin>> {
+        for plugin in &self.in_plugins {
+            if plugin.get_name() == name {
+                return Some(plugin);
+            }
+        }
+        None
+    }
+
+    pub fn get_type_out(&self, name: &String) -> Option<&Box<dyn OutputPlugin>> {
+        for plugin in &self.out_plugins {
+            if plugin.get_name() == name {
+                return Some(plugin);
+            }
+        }
+        None
+    }
+
     fn load_default_plugins() -> (Vec<Box<dyn InputPlugin>>, Vec<Box<dyn OutputPlugin>>) {
         let in_plugins: Vec<Box<dyn InputPlugin>> = vec!(
             input::DebugPlugin::new()
