f5325e0960e4e5dbc146cf0646acf4cf0c87858d
NakagamiYuta <guguru0014@gmail.com>
2022-06-30 03:43:47 +0900

    [chagne](command/cli) CliCmd::parse_cli_textの仕様を変更

--- src/command/cli.rs
+++ src/command/cli.rs
@@ -85,13 +85,13 @@ impl CliCmd {
     fn parse_cli_text(text: String) -> Option<(String, String, Vec<String>)> {
         let splitted_text = text.split(" ").map(|s| s.to_string()).collect::<Vec<String>>();
         match splitted_text.len() {
-            0 | 1 => None,
-            2 => {
-                if splitted_text[1].len() == 0 {
+            1 => {
+                if splitted_text[0].len() == 0 {
                     return None;
                 }
-                Some((splitted_text[0].clone(), splitted_text[1].clone(), vec!()))
-            }
+                Some((splitted_text[0].clone(), "".to_string(), vec!()))
+            },
+            2 => Some((splitted_text[0].clone(), splitted_text[1].clone(), vec!())),
             _ => Some((splitted_text[0].clone(), splitted_text[1].clone(), splitted_text[2..].to_vec()))
         }
     } 
@@ -104,22 +104,31 @@ mod test {
     #[test]
     fn test_parse_cli_text() {
         assert!(CliCmd::parse_cli_text("".to_string()).is_none());
-        assert!(CliCmd::parse_cli_text("aaa".to_string()).is_none());
+
+        match CliCmd::parse_cli_text("aaa".to_string()) {
+            Some((cmd, subcmd, args)) => {
+                let empty_string_vec: Vec<String> = vec!();
+                assert_eq!(cmd, "aaa");
+                assert_eq!(subcmd, "");
+                assert_eq!(args, empty_string_vec);
+            },
+            None => assert!(false)
+        }
 
         match CliCmd::parse_cli_text("aaa bbb".to_string()) {
-            Some((mode, name, args)) => {
+            Some((cmd, subcmd, args)) => {
                 let empty_string_vec: Vec<String> = vec!();
-                assert_eq!(mode, "aaa");
-                assert_eq!(name, "bbb");
+                assert_eq!(cmd, "aaa");
+                assert_eq!(subcmd, "bbb");
                 assert_eq!(args, empty_string_vec);
             },
             None => assert!(false)
         } 
 
         match CliCmd::parse_cli_text("aaa bbb ccc".to_string()) {
-            Some((mode, name, args)) => {
-                assert_eq!(mode, "aaa");
-                assert_eq!(name, "bbb");
+            Some((cmd, subcmd, args)) => {
+                assert_eq!(cmd, "aaa");
+                assert_eq!(subcmd, "bbb");
                 assert_eq!(args, vec!("ccc".to_string()));
             },
             None => assert!(false)
