84b3e4d5a4179ef403ef1ccc0ca37edf4551c713
NakagamiYuta <guguru0014@gmail.com>
2022-08-28 21:04:34 +0900

    [add](compiler/types) u32, f32, bool, char追加

--- src/compiler/types.rs
+++ src/compiler/types.rs
@@ -36,6 +36,10 @@ impl Type {
 pub enum TypeKind {
     /* プリミティブ型 */
     Int32,
+    UInt32,
+    Float32,
+    Boolean,
+    Char,
 
     /* ユーザ定義型 */
     Data,
@@ -49,7 +53,22 @@ impl TypeKind {
     fn from(name: String) -> TypeKind {
         match name.as_str() {
             "i32" => TypeKind::Int32,
+            "u32" => TypeKind::UInt32,
+            "f32" => TypeKind::Float32,
+            "bool" => TypeKind::Boolean,
+            "char" => TypeKind::Char,
             _ => TypeKind::Unsolved(name)
+        }
+    }
+
+    pub fn is_primitive(&self) -> bool {
+        match self {
+            TypeKind::Int32 |
+            TypeKind::UInt32 |
+            TypeKind::Float32 |
+            TypeKind::Boolean |
+            TypeKind::Char => true,
+            _ => false
         }
     }
 }
@@ -58,6 +77,10 @@ impl Debug for TypeKind {
     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
         match self {
             TypeKind::Int32 => write!(f, "i32"),
+            TypeKind::UInt32 => write!(f, "u32"),
+            TypeKind::Float32 => write!(f, "f32"),
+            TypeKind::Boolean => write!(f, "bool"),
+            TypeKind::Char => write!(f, "char"),
             TypeKind::Data => write!(f, "Data"),
             TypeKind::Unsolved(hint) => write!(f, "{}", hint),
             TypeKind::UnsolvedNoHint => write!(f, "UnsolvedNoHint"),
@@ -111,6 +134,10 @@ mod test {
     #[test]
     fn primitive() {
         check_serialize!(TypeKind, TypeKind::Int32);
+        check_serialize!(TypeKind, TypeKind::UInt32);
+        check_serialize!(TypeKind, TypeKind::Float32);
+        check_serialize!(TypeKind, TypeKind::Boolean);
+        check_serialize!(TypeKind, TypeKind::Char);
         check_serialize!(TypeKind, TypeKind::Data);
     }
 
