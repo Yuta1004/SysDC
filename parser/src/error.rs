ccbc1a9fbd1970dd1843e69d7db7a690ec7ffe9c
NakagamiYuta <guguru0014@gmail.com>
2022-09-06 15:27:56 +0900

    [change](parser/error) PErrorKindのDisplay実装をPErrorに移した

--- parser/src/error.rs
+++ parser/src/error.rs
@@ -36,34 +36,6 @@ pub enum PErrorKind {
     IllegalAccess,
 }
 
-impl Display for PErrorKind {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        match self {
-            PErrorKind::RequestedTokenNotFound(kind) => write!(f, "Token \"{:?}\" is requested, but not found", kind),
-            PErrorKind::FoundUnregisteredSymbol => write!(f, "Found unregistered symbol"),
-
-            PErrorKind::UnitNameNotSpecified => write!(f, "Unit name is not specified"),
-            PErrorKind::FromNamespaceNotSpecified => write!(f, "From namespace is not specified"),
-            PErrorKind::DataOrModuleNotFound => write!(f, "Expected Data or Module definition, but not found"),
-            PErrorKind::UnexpectedEOF => write!(f, "Unexpected EOF found"),
-            PErrorKind::ReturnExistsMultiple => write!(f, "Annotation \"return\" exists multiple"),
-            PErrorKind::ReturnNotExists => write!(f, "Annotation \"return\" not existed"),
-            PErrorKind::ResultOfSpawnNotSpecified => write!(f, "Missing to specify the result of spawn"),
-            PErrorKind::FunctionNameNotFound => write!(f, "Function name is requested, but not found"),
-
-            PErrorKind::AlreadyDefined(name) => write!(f, "\"{}\" is already defined", name),
-            PErrorKind::TypeUnmatch1(actual) => write!(f, "\"{:?}\" is defined, but type is unmatch", actual),
-            PErrorKind::TypeUnmatch2(required, actual) => write!(f, "\"{:?}\" is required, but \"{:?}\" exists", required, actual),
-            PErrorKind::NotFound(name) => write!(f, "Cannot find \"{}\"", name),
-            PErrorKind::NotDefined(name) => write!(f, "\"{}\" is not defined", name),
-            PErrorKind::MemberNotDefinedInData(member, data) => write!(f, "Member \"{}\" is not defined in Data \"{}\"", member, data),
-            PErrorKind::FuncNotDefinedInModule(func, module) => write!(f, "Function \"{}\" is not defined in Module \"{}\"", func, module),
-            PErrorKind::MissingFunctionName => write!(f, "Missing to specify the function"),
-            PErrorKind::IllegalAccess => write!(f, "Found illegal access"),
-        }
-    }
-}
-
 impl PErrorKind {
     pub fn to_err<T>(self) -> PResult<T> {
         Err(PError {
@@ -87,7 +59,30 @@ impl Error for PError {}
 
 impl Display for PError {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{} (at {})", self.kind, self.happen_at)
+        match &self.kind {
+            PErrorKind::RequestedTokenNotFound(kind) => write!(f, "Token \"{:?}\" is requested, but not found", kind),
+            PErrorKind::FoundUnregisteredSymbol => write!(f, "Found unregistered symbol"),
+
+            PErrorKind::UnitNameNotSpecified => write!(f, "Unit name is not specified"),
+            PErrorKind::FromNamespaceNotSpecified => write!(f, "From namespace is not specified"),
+            PErrorKind::DataOrModuleNotFound => write!(f, "Expected Data or Module definition, but not found"),
+            PErrorKind::UnexpectedEOF => write!(f, "Unexpected EOF found"),
+            PErrorKind::ReturnExistsMultiple => write!(f, "Annotation \"return\" exists multiple"),
+            PErrorKind::ReturnNotExists => write!(f, "Annotation \"return\" not existed"),
+            PErrorKind::ResultOfSpawnNotSpecified => write!(f, "Missing to specify the result of spawn"),
+            PErrorKind::FunctionNameNotFound => write!(f, "Function name is requested, but not found"),
+
+            PErrorKind::AlreadyDefined(name) => write!(f, "\"{}\" is already defined", name),
+            PErrorKind::TypeUnmatch1(actual) => write!(f, "\"{:?}\" is defined, but type is unmatch", actual),
+            PErrorKind::TypeUnmatch2(required, actual) => write!(f, "\"{:?}\" is required, but \"{:?}\" exists", required, actual),
+            PErrorKind::NotFound(name) => write!(f, "Cannot find \"{}\"", name),
+            PErrorKind::NotDefined(name) => write!(f, "\"{}\" is not defined", name),
+            PErrorKind::MemberNotDefinedInData(member, data) => write!(f, "Member \"{}\" is not defined in Data \"{}\"", member, data),
+            PErrorKind::FuncNotDefinedInModule(func, module) => write!(f, "Function \"{}\" is not defined in Module \"{}\"", func, module),
+            PErrorKind::MissingFunctionName => write!(f, "Missing to specify the function"),
+            PErrorKind::IllegalAccess => write!(f, "Found illegal access"),
+        }?;
+        write!(f, " (at {})", self.happen_at)
     }
 }
 
