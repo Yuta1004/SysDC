9dd24b0c0748470550d056b10c04585a151797d8
NakagamiYuta <guguru0014@gmail.com>
2022-09-19 20:19:10 +0900

    [fix](tools/view) Edgeが隠れる問題を修正

--- tools/view/src/components/flow.tsx
+++ tools/view/src/components/flow.tsx
@@ -14,6 +14,7 @@ export function FlowComponent({nodes, edges}) {
         <ReactFlow
             nodes={nodes}
             edges={edges}
+            defaultEdgeOptions={{ zIndex: 9999 }}
             fitView
         />
     );
