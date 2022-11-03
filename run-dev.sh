b6993658c79e1709e7fbf9cd48c9c5f2fba53430
NakagamiYuta <guguru0014@gmail.com>
2022-11-03 16:54:09 +0900

    [add] サーバ起動用シェルスクリプト作成

---
+++ run-dev.sh
@@ -0,0 +1,4 @@
+export SYSDC_PORT=50000
+export SYSDC_HUGO_OPTIONS=""
+
+docker-compose up
---
+++ run.sh
@@ -0,0 +1,4 @@
+export SYSDC_PORT=50000
+export SYSDC_HUGO_OPTIONS="--disableLiveReload --appendPort=false"
+
+docker-compose up
