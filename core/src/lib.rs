c327e54e6e3387f8933315ea6cec1f05c51e9064
NakagamiYuta <guguru0014@gmail.com>
2022-11-03 19:16:18 +0900

    [add](core) wasm対応

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -3,30 +3,44 @@ mod token;
 mod check;
 mod error;
 mod location;
-pub mod name;
-pub mod types;
-pub mod structure;
+mod name;
+mod types;
+mod structure;
+
+use wasm_bindgen::JsValue;
+use wasm_bindgen::prelude::wasm_bindgen;
 
 use parse::UnitParser;
 use structure::unchecked;
-use structure::SysDCSystem;
 use token::Tokenizer;
 
+macro_rules! q {
+    ($target:expr) => {
+        match $target {
+            Ok(target) => target,
+            Err(err) => return Err(err.to_string())
+        }
+    };
+}
+
+#[wasm_bindgen]
 #[derive(Default)]
 pub struct Parser {
     units: Vec<unchecked::SysDCUnit>,
 }
 
+#[wasm_bindgen]
 impl Parser {
-    pub fn parse(&mut self, filename: String, program: &str) -> anyhow::Result<()> {
+    pub fn parse(&mut self, filename: String, program: &str) -> Result<(), String> {
         let tokenizer = Tokenizer::new(filename, program);
-        let system = UnitParser::parse(tokenizer)?;
-        self.units.push(system);
+        let unit = q!(UnitParser::parse(tokenizer));
+        self.units.push(unit);
         Ok(())
     }
 
-    pub fn check(self) -> anyhow::Result<SysDCSystem> {
+    pub fn check(self) -> Result<JsValue, String> {
         let system = unchecked::SysDCSystem::new(self.units);
-        check::check(system)
+        let system = q!(check::check(system));
+        Ok(serde_wasm_bindgen::to_value(&system).unwrap())
     }
 }
