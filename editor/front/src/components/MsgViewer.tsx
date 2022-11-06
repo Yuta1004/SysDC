2379506b538293281e15669eea67c9c7c0783e9e
NakagamiYuta <guguru0014@gmail.com>
2022-11-07 05:02:36 +0900

    [fix](editor/front) メッセージ消滅時に空のSnackbarが残る挙動に対処

--- editor/front/src/components/MsgViewer.tsx
+++ editor/front/src/components/MsgViewer.tsx
@@ -10,15 +10,29 @@ const MsgViewer = () => {
 
     return (<>
         <Snackbar
-            open={ type !== "" }
+            open={ type === "success" }
             autoHideDuration={6000}
-            onClose={ () => showMsg(["", ""]) }
-            anchorOrigin={{ vertical: "top", horizontal: "center"}}
+            onClose={ () => showMsg(["", msg]) }
+            anchorOrigin={{ vertical: "top", horizontal: "center" }}
             sx={{ zIndex: 9999 }}
         >
             <Alert
-                onClose={ () => showMsg(["", ""]) }
-                severity={ "success" === type ? type : "error" }
+                onClose={ () => showMsg(["", msg]) }
+                severity="success"
+            >
+                { msg }
+            </Alert>
+        </Snackbar>
+        <Snackbar
+            open={ type === "error" }
+            autoHideDuration={6000}
+            onClose={ () => showMsg(["", msg]) }
+            anchorOrigin={{ vertical: "top", horizontal: "center" }}
+            sx={{ zIndex: 9999 }}
+        >
+            <Alert
+                onClose={ () => showMsg(["", msg]) }
+                severity="error"
             >
                 { msg }
             </Alert>
