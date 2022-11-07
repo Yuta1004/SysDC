d74b21b8f9eff252f100f0803f75319842e6cec3
NakagamiYuta <guguru0014@gmail.com>
2022-11-07 17:35:53 +0900

    [add](tool/delivery) Loggerを追加

--- tool/delivery/src/main.rs
+++ tool/delivery/src/main.rs
@@ -1,10 +1,13 @@
 mod s3;
-use actix_web::{get, App, HttpServer, Responder};
+use actix_web::middleware::Logger;
+use actix_web::{get, App, HttpRequest, HttpServer, Responder};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    println!("listen at -> 0.0.0.0:50000");
-    HttpServer::new(|| App::new().service(root))
+    env_logger::init();
+    HttpServer::new(||
+            App::new().service(root).wrap(Logger::default())
+        )
         .bind(("0.0.0.0", 50000))?
         .run()
         .await
