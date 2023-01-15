321a63a7890a7854c72c66edb9624c7546c88984
NakagamiYuta <guguru0014@gmail.com>
2023-01-16 07:39:59 +0900

    [change](std/tool/eval) Advice構造体にSerialize, Deserializeを載せた

--- std/tool/eval/wasm/src/lib.rs
+++ std/tool/eval/wasm/src/lib.rs
@@ -1,23 +1,26 @@
+pub mod commands;
+
 #[cfg(feature = "wasm")]
 use wasm_bindgen::prelude::wasm_bindgen;
 
-#[derive(Debug, Clone)]
-#[cfg_attr(feature = "wasm", wasm_bindgen)]
+use serde::{ Serialize, Deserialize };
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum AdviceLevel {
     Info = 0,
     Warning = 1
 }
 
-#[derive(Debug, Clone)]
-#[cfg_attr(feature = "wasm", wasm_bindgen)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Advice {
     level: AdviceLevel,
     title: String,
-    message: String
+    message: Vec<String>
 }
 
+
 impl Advice {
-    pub fn new(level: AdviceLevel, title: String, message: String) -> Advice {
+    pub fn new(level: AdviceLevel, title: String, message: Vec<String>) -> Advice {
         Advice { level, title, message }
     }
 }
