f10c7f2a8f71a45f1a4b19437e6cb7ae71c3e5c0
NakagamiYuta <guguru0014@gmail.com>
2023-01-16 11:04:10 +0900

    [add](std/tool/eval) とりあえず定義済モジュール数などを表示できるように

--- std/tool/eval/wasm/src/commands/basic.rs
+++ std/tool/eval/wasm/src/commands/basic.rs
@@ -3,7 +3,12 @@ use sysdc_core::structure::SysDCSystem;
 
 #[cfg_attr(not(feature = "wasm"), allow(dead_code))]
 pub fn eval_basic_stat(system: &SysDCSystem) -> Vec<Advice> {
-    let messages = vec!["aaa".to_string()];
+    let mut messages = vec![];
+
+    let [unit_cnt, module_cnt, func_cnt] = count_elements(system);
+    messages.push(format!("ユニット : {} 定義済", unit_cnt));
+    messages.push(format!("モジュール : {} 定義済", module_cnt));
+    messages.push(format!("関数 : {} 定義済", func_cnt));
 
     vec![
         Advice::new(
@@ -12,4 +17,18 @@ pub fn eval_basic_stat(system: &SysDCSystem) -> Vec<Advice> {
             messages
         )
     ]
+}
+
+fn count_elements(system: &SysDCSystem) -> [i32; 3] {
+    let unit_cnt = system.units.len() as i32;
+
+    let other_cnt = system.units.iter().fold([0, 0], |cnt, unit| {
+        let module_cnt = cnt[0] + unit.modules.len() as i32;
+        let func_cnt = unit.modules.iter().fold(cnt[1], |cnt, module| {
+            cnt + module.functions.len() as i32
+        });
+        [module_cnt, func_cnt]
+    });
+
+    [unit_cnt, other_cnt[0], other_cnt[1]]  // unit, module, function
 }
