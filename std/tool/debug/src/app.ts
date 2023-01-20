041620048e4f8bec3fa4144311a00ffbdf3b6667
NakagamiYuta <guguru0014@gmail.com>
2023-01-21 01:05:29 +0900

    [change](std/tool/debug) Editor側から送信されるsystemの型の変更に対応

--- std/tool/debug/src/app.ts
+++ std/tool/debug/src/app.ts
@@ -3,6 +3,6 @@ window.onmessage = (event) => app(event.data);
 const app = (system: string) => {
     const view = document.getElementById("view");
     if (view !== null) {
-        view.innerText = system;
+        view.innerText = JSON.stringify(system);
     }
 }
