e9adb81e029c8550e2a90801ac523e1418986844
NakagamiYuta <guguru0014@gmail.com>
2023-01-21 06:59:09 +0900

    [update](cliapp) cliappのビルドシステムを整備

--- cliapp/tool_runner/src-tauri/build.rs
+++ cliapp/tool_runner/src-tauri/build.rs
@@ -1,18 +1,3 @@
-use std::process::Command;
-
 fn main() {
-    if option_env!("DONT_BUILD_REACT").is_none() {
-        Command::new("npm")
-            .args(&["run", "build"])
-            .current_dir("../")
-            .status()
-            .unwrap();
-    }
-
     tauri_build::build();
-
-    println!("cargo:rerun-if-changed=../dist");
-    println!("cargo:rerun-if-changed=../src");
-    println!("cargo:rerun-if-changed=../node_modules");
-    println!("cargo:rerun-if-changed=../package.json");
 }
