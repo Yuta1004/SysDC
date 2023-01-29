ce684b1d43cfe73fd06ab9a54483da2866341ea6
addtobasic <addtobasic@gmail.com>
2023-01-29 14:11:48 +0900

    [add] ogp画像の設定の追加

--- webapp/editor/front/src/index.tsx
+++ webapp/editor/front/src/index.tsx
@@ -1,13 +1,25 @@
+import "normalize.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Helmet } from "react-helmet";
 import App from "./App";
-import "normalize.css";
 
 const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Helmet>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+      <meta
+        property="og:url"
+        content="%PUBLIC_URL%/sysdc-ogp.png"
+      />
+      <link rel="icon" href="%PUBLIC_URL%/favicon.png" />
+      <title>SysDC Editor</title>
+    </Helmet>
+    <App />
+  </React.StrictMode>
 );
